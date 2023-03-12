f5d330810098b2f7a111de58e61489e271451420
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2023-03-12 19:04:54 +0100

    add: create browser router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
+import {
+  createBrowserRouter,
+  Route,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+
 import "./App.css";
+import RootLayout from "./layouts/RootLayout";
+import About from "./pages/About";
+import Home from "./pages/Home";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+    </Route>
+  )
+);
 
 function App() {
-  return <div className="App"></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
